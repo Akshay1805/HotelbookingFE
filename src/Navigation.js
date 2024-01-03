1a522905806241f05be08285042f16e3500589ea
Akshay1805 <akshayjegan@outlook.com>
2024-01-03 23:12:41 +0530

    first commit

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,13 +1,15 @@
 // Navigation.js
 
 import React from 'react';
-import { Link } from 'react-router-dom';
+
 
 function Navigation() {
+    window.
   return (
     <nav>
       <ul>
         <li><Link to="/home">Home</Link></li>
+        <li><Link to="/login">Home</Link></li>
         {/* Add more navigation links as needed */}
       </ul>
     </nav>
