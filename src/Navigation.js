ea1c303de69fbcaacb48c403a571a4a243114a46
Akshay1805 <akshayjegan@outlook.com>
2024-01-03 23:16:38 +0530

    first

--- src/Navigation.js
+++ src/Navigation.js
@@ -3,14 +3,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-
 function Navigation() {
-    window.
   return (
     <nav>
       <ul>
         <li><Link to="/home">Home</Link></li>
-        <li><Link to="/login">Home</Link></li>
+        <li><Link to="/login">Login</Link></li>
         {/* Add more navigation links as needed */}
       </ul>
     </nav>
